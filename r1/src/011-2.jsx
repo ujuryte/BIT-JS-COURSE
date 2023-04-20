0b27b0775176d4c94178e9ce9e529af0f29c0ac6
ujuryte <ujuryte@gmail.com>
2023-04-20 15:30:42 +0300

    useRef

---
+++ r1/src/011-2.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import './App.scss';
+import ContentEditable from 'react-contenteditable'
+
+const list = [
+    { id: 123, name: 'Beaver' },
+    { id: 124, name: 'Fox' },
+    { id: 125, name: 'Rabbit' },
+    { id: 126, name: 'Wolf' }
+]
+
+
+function App() {
+
+    const [text, setText] = useState({
+        t1: 'Labas',
+        t2: 'Ate'
+    })
+
+    const [checkbox, setCheckbox] = useState({
+        A: true,
+        B: false,
+        C: false,
+        D: true
+    })
+
+    const [radio, setRadio] = useState('C');
+
+    const [select, setSelect] = useState({open: false, data: 124});
+
+    const handleChange1 = (e, t) => {
+        setText(texts => ({ ...texts, [t]: e.target.value }))
+    }
+
+    const handleChange2 = (cb) => {
+        setCheckbox(checkboxes => ({ ...checkboxes, [cb]: !checkboxes[cb] }))
+    }
+
+    const handleChange3 = r => {
+        setRadio(r)
+    }
+
+    const handleChange4 = id => {
+        setSelect(_ => ({open: false, data: parseInt(id)}));
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Formos</h1>
+                <fieldset>
+                    <legend>Few texts one state</legend>
+                    <div>
+                        <ContentEditable tagName='i' onChange={e => handleChange1(e, 't1')} html={text.t1} />
+                    </div>
+                    <div>
+                        <ContentEditable tagName='u' onChange={e => handleChange1(e, 't2')} html={text.t2} />
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>4 checkboxes one state</legend>
+                    <div className='field'>
+                        <span className={'checkbox' + (checkbox.A ? ' active' : '')} onClick={_ => handleChange2('A')}>A</span>
+                        <span className={'checkbox' + (checkbox.B ? ' active' : '')} onClick={_ => handleChange2('B')}>B</span>
+                        <span className={'checkbox' + (checkbox.C ? ' active' : '')} onClick={_ => handleChange2('C')}>C</span>
+                        <span className={'checkbox' + (checkbox.D ? ' active' : '')} onClick={_ => handleChange2('D')}>D</span>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>4 checkboxes one radio</legend>
+                    <div className='field'>
+                        <span className={'checkbox' + (radio === 'A' ? ' active' : '')} onClick={_ => handleChange3('A')}>A</span>
+                        <span className={'checkbox' + (radio === 'B' ? ' active' : '')} onClick={_ => handleChange3('B')}>B</span>
+                        <span className={'checkbox' + (radio === 'C' ? ' active' : '')} onClick={_ => handleChange3('C')}>C</span>
+                        <span className={'checkbox' + (radio === 'D' ? ' active' : '')} onClick={_ => handleChange3('D')}>D</span>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Select on one state</legend>
+                    <div className='select'>
+                        <div className='selected' onClick={_ => setSelect(s => ({...s, open: !s.open}))}>{list.find(l => select.data == l.id).name}</div>
+                        <div className='list' style={{height: select.open ? '140px' : 0}} >
+                            {
+                                list.map(o => <div key={o.id} onClick={_ => handleChange4(o.id)}>{o.name}</div>)
+                            }
+                        </div>
+                    </div>
+                </fieldset>
+
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,96 +1,54 @@
 import { useState } from 'react';
 import './App.scss';
-import ContentEditable from 'react-contenteditable'
-
-const list = [
-    { id: 123, name: 'Beaver' },
-    { id: 124, name: 'Fox' },
-    { id: 125, name: 'Rabbit' },
-    { id: 126, name: 'Wolf' }
-]
-
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
-    const [text, setText] = useState({
-        t1: 'Labas',
-        t2: 'Ate'
-    })
+    const [count, setCount] = useState(1);
+    
+    const mySave = useRef(1);
 
-    const [checkbox, setCheckbox] = useState({
-        A: true,
-        B: false,
-        C: false,
-        D: true
-    })
+    const needFocus = useRef();
 
-    const [radio, setRadio] = useState('C');
+    // const [mySave2, setMySave2] =useState(1);
+    
+    useEffect(() => {
 
-    const [select, setSelect] = useState({open: false, data: 124});
+        mySave.current = count;
+        console.log('SAVE', mySave.current)
 
-    const handleChange1 = (e, t) => {
-        setText(texts => ({ ...texts, [t]: e.target.value }))
+    }, [count, mySave])
+
+    // useEffect(() => {
+
+    //     setMySave2(count);
+    //     console.log('SAVE2', mySave2)
+
+    // }, [count, mySave2])
+
+    const click = _ => {
+        setCount(c => c + 1);
+        // document.querySelector('#need-focus').focus();
+        needFocus.current.focus()
+        console.log('CLICK', mySave.current)
     }
 
-    const handleChange2 = (cb) => {
-        setCheckbox(checkboxes => ({ ...checkboxes, [cb]: !checkboxes[cb] }))
-    }
-
-    const handleChange3 = r => {
-        setRadio(r)
-    }
-
-    const handleChange4 = id => {
-        setSelect(_ => ({open: false, data: parseInt(id)}));
-    }
+    // const save = _ => {
+    //     mySave.current = count;
+    //     console.log('SAVE', mySave.current)
+    // }
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Formos</h1>
-                <fieldset>
-                    <legend>Few texts one state</legend>
-                    <div>
-                        <ContentEditable tagName='i' onChange={e => handleChange1(e, 't1')} html={text.t1} />
-                    </div>
-                    <div>
-                        <ContentEditable tagName='u' onChange={e => handleChange1(e, 't2')} html={text.t2} />
-                    </div>
-                </fieldset>
+                <h1>useRef</h1>
 
-                <fieldset>
-                    <legend>4 checkboxes one state</legend>
-                    <div className='field'>
-                        <span className={'checkbox' + (checkbox.A ? ' active' : '')} onClick={_ => handleChange2('A')}>A</span>
-                        <span className={'checkbox' + (checkbox.B ? ' active' : '')} onClick={_ => handleChange2('B')}>B</span>
-                        <span className={'checkbox' + (checkbox.C ? ' active' : '')} onClick={_ => handleChange2('C')}>C</span>
-                        <span className={'checkbox' + (checkbox.D ? ' active' : '')} onClick={_ => handleChange2('D')}>D</span>
-                    </div>
-                </fieldset>
+                <input id="need-focus" ref={needFocus} type="text" value={count}></input>
 
-                <fieldset>
-                    <legend>4 checkboxes one radio</legend>
-                    <div className='field'>
-                        <span className={'checkbox' + (radio === 'A' ? ' active' : '')} onClick={_ => handleChange3('A')}>A</span>
-                        <span className={'checkbox' + (radio === 'B' ? ' active' : '')} onClick={_ => handleChange3('B')}>B</span>
-                        <span className={'checkbox' + (radio === 'C' ? ' active' : '')} onClick={_ => handleChange3('C')}>C</span>
-                        <span className={'checkbox' + (radio === 'D' ? ' active' : '')} onClick={_ => handleChange3('D')}>D</span>
-                    </div>
-                </fieldset>
+                <button className='red' onClick={click}>Click Me</button>
 
-                <fieldset>
-                    <legend>Select on one state</legend>
-                    <div className='select'>
-                        <div className='selected' onClick={_ => setSelect(s => ({...s, open: !s.open}))}>{list.find(l => select.data == l.id).name}</div>
-                        <div className='list' style={{height: select.open ? '140px' : 0}} >
-                            {
-                                list.map(o => <div key={o.id} onClick={_ => handleChange4(o.id)}>{o.name}</div>)
-                            }
-                        </div>
-                    </div>
-                </fieldset>
-
-
+                {/* <button className='blue' onClick={save}>Save</button> */}
             </header>
         </div>
     );
