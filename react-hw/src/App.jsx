db414b91b190b0d8e9bb35d701dc8c502db4c40e
ujuryte <ujuryte@gmail.com>
2023-04-24 12:05:29 +0300

    hw

--- react-hw/src/App.jsx
+++ react-hw/src/App.jsx
@@ -1,18 +1,34 @@
-
+import { useState } from 'react';
 import './App.scss';
-import Ganykla from './Components/04-13/Ganykla';
-
+import Azuolas from './Components/04-12/Azuolas';
+import Berzas from './Components/04-12/Berzas';
+import Uosis from './Components/04-12/Uosis';
 
 
 
 function App() {
 
+const [tree, setTree] = useState([])
+
+const azuol = _ => {
+    setTree(s => [...s, {type: 'azuolas', show: true}])
+}
 
     return (
         <div className="App">
             <header className="App-header">
-            <Ganykla />
-                
+
+                <div className='field'>
+                    <Azuolas tree={tree}/>
+                    <Berzas />
+                    <Uosis />
+                </div>
+                <div>
+                    <button className='red' onClick={azuol}>Azuolas</button>
+                    <button className='red'>Berzas</button>
+                    <button className='red'>Uosis</button>
+                </div>
+
             </header>
         </div>
     );
